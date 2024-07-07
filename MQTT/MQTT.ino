b177833b60152ce5d2dc33b5e4fbd8ea14c7b3e1
danghoang03 <dang.hoang1205@hcmut.edu.vn>
2024-07-07 14:15:18 +0700

    Controll LED with MQTT

--- MQTT/MQTT.ino
+++ MQTT/MQTT.ino
@@ -5,28 +5,32 @@
 const char WIFI_SSID[] = "ACLAB";     // CHANGE TO YOUR WIFI SSID
 const char WIFI_PASSWORD[] = "ACLAB2023";  // CHANGE TO YOUR WIFI PASSWORD
 
-const char MQTT_BROKER_ADRRESS[] = "test.mosquitto.org";  // CHANGE TO MQTT BROKER'S ADDRESS
+const char MQTT_BROKER_ADRRESS[] = "broker.hivemq.com";  // CHANGE TO MQTT BROKER'S ADDRESS
 const int MQTT_PORT = 1883;
-const char MQTT_CLIENT_ID[] = "Dang-esp32-001";  
+const char MQTT_CLIENT_ID[] = "Dang-esp32-mqtt";  
 
-const char MQTT_USERNAME[] = "";                        // CHANGE IT IF REQUIRED, empty if not required
-const char MQTT_PASSWORD[] = "";                        // CHANGE IT IF REQUIRED, empty if not required
+const char MQTT_USERNAME[] = "HaiDang";                        // CHANGE IT IF REQUIRED, empty if not required
+const char MQTT_PASSWORD[] = "DangHai";                        // CHANGE IT IF REQUIRED, empty if not required
 
 // The MQTT topics that ESP32 should publish/subscribe
-const char PUBLISH_TOPIC[] = "Dang-esp32-001/loopback";    
-const char SUBSCRIBE_TOPIC[] = "Dang-esp32-001/loopback";  
+const char PUBLISH_TOPIC[] = "Dang/Led";    
+const char SUBSCRIBE_TOPIC[] = "Dang/Led";  
 
-const int PUBLISH_INTERVAL = 5000;  // 5 seconds
+//const int PUBLISH_INTERVAL = 5000;  // 5 seconds
 
 WiFiClient network;
 MQTTClient mqtt = MQTTClient(256);
 
-unsigned long lastPublishTime = 0;
+//unsigned long lastPublishTime = 0;
+
+//char messageBuff[100]; //use to store message
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-   WiFi.mode(WIFI_STA);
+  pinMode(D13, OUTPUT);
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("ESP32 - Connecting to Wi-Fi");
@@ -42,11 +46,6 @@ void setup() {
 
 void loop() {
   mqtt.loop();
-
-  if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
-    sendToMQTT();
-    lastPublishTime = millis();
-  }
 }
 
 void connectToMQTT() {
@@ -79,25 +78,29 @@ void connectToMQTT() {
   Serial.println("ESP32 - MQTT broker Connected!");
 }
 
-void sendToMQTT() {
-  StaticJsonDocument<200> message;
-  message["timestamp"] = millis();
-  message["data"] = analogRead(0);  // Or you can read data from other sensors
-  char messageBuffer[512];
-  serializeJson(message, messageBuffer);
+// void sendToMQTT() {
+//   StaticJsonDocument<200> message;
+//   message["timestamp"] = millis();
+//   message["data"] = analogRead(0);  // Or you can read data from other sensors
+//   char messageBuffer[512];
+//   serializeJson(message, messageBuffer);
 
-  mqtt.publish(PUBLISH_TOPIC, messageBuffer);
+//   mqtt.publish(PUBLISH_TOPIC, messageBuffer);
 
-  Serial.println("ESP32 - sent to MQTT:");
-  Serial.print("- topic: ");
-  Serial.println(PUBLISH_TOPIC);
-  Serial.print("- payload:");
-  Serial.println(messageBuffer);
-}
+//   Serial.println("ESP32 - sent to MQTT:");
+//   Serial.print("- topic: ");
+//   Serial.println(PUBLISH_TOPIC);
+//   Serial.print("- payload:");
+//   Serial.println(messageBuffer);
+// }
 
 void messageHandler(String &topic, String &payload) {
-  Serial.println("ESP32 - received from MQTT:");
-  Serial.println("- topic: " + topic);
-  Serial.println("- payload:");
-  Serial.println(payload);
+  if (payload == "on"){
+    digitalWrite(D13, HIGH);
+    mqtt.publish(PUBLISH_TOPIC, "LED is turn on");
+  }
+  else if(payload == "off"){
+    digitalWrite(D13, LOW);
+    mqtt.publish(PUBLISH_TOPIC, "LED is turn off");
+  }
 }
